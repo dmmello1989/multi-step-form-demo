68ef894c2405f976ed285950c689866e7c1b2658
dmmello1989 <dmmello1989@gmail.com>
2023-06-24 14:41:55 -0300

    WIP finishNoteStep

---
+++ src/components/formSection/finishNoteStep/index.tsx
@@ -0,0 +1,14 @@
+import { FormWrapper } from "../formWrapper/index.js";
+import * as S from "../styles.js";
+
+export const FinishNoteStep = () => {
+  return (
+    <FormWrapper
+      isColumn
+      title="Finishing up"
+      subtitle="Double-check everything looks OK before confirming."
+    >
+        
+    </FormWrapper>
+  )
+}
